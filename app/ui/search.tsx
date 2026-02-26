b9f5aff01a075d1d987997a1d572566a31a4ff48
Areesh18 <areeshzafar396@gmail.com>
2026-02-26 15:54:14 +0530

    implemented debouncing using the use-debounce package

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,20 +1,21 @@
 "use client";
-import { useSearchParams,usePathname, useRouter } from "next/navigation";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+
     const params = new URLSearchParams(searchParams);
     if (term) {
-      params.set('query', term);
+      params.set("query", term);
     } else {
-      params.delete('query');
+      params.delete("query");
     }
     replace(`${pathname}?${params.toString()}`);
-    console.log(`Searching... ${term}`);
-  }
-  const searchParams=useSearchParams();
+  }, 300);
+  const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
   return (
@@ -28,7 +29,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
